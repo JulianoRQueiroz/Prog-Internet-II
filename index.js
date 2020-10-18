8c97aa84934ac057143847ad2e02be023fec51f3
JulianoQueiroz <68660839+JulianoQueiroz@users.noreply.github.com>
2020-10-18 16:47:33 -0300

    api restfull

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const requireDir = require('require-dir');
 
 //Inicializando o app
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 // Iniciando o BD (conexão)
 mongoose.connect(
