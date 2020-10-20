46fa89e1890cb1eec6da5ef14a05510f13b9a123
JulianoQueiroz <68660839+JulianoQueiroz@users.noreply.github.com>
2020-10-20 15:11:26 -0300

    Update User.js

--- src/models/User.js
+++ src/models/User.js
@@ -17,7 +17,7 @@ const UserSchema = new mongoose.Schema({
         require: true,
         select: false //sem campo senha
     },
-    createAt: {
+    criaData: {
         type: Date,
         default: Date.now
     }
